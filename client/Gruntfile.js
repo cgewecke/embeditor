039e6665a1511f8d50fa15e79bbad8f46fe835fb
cgewecke <christophergewecke@gmail.com>
2015-04-27 02:55:49 -0700

    Completed autocomplete. Code & unit tests. First draft of youTubeDataAPI Service

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -412,7 +412,7 @@ module.exports = function (grunt) {
     karma: {
       unit: {
         configFile: 'test/karma.conf.js',
-        singleRun: true
+        singleRun: false
       }
     }
   });
--- client/test/spec/controllers/searchbox.js
+++ client/test/spec/controllers/searchbox.js
@@ -1,33 +1,75 @@
 'use strict';
 
+var debug_I, debug_II;
+
 describe('Controller: SearchboxCtrl', function () {
 
   // load the controller's module
   beforeEach(module('embeditor'));
 
-  beforeEach(inject( function($controller, $rootScope, youTubeDataAPI ) {
+  describe('submitting a search query via md-autocomplete form', function(){
+
+    var scope, ctrl, form, searchButton, event;
+
+    beforeEach(inject( function($controller, $rootScope, $compile ) {
+
       scope = $rootScope.$new();
-      ctrl = $controller("SearchboxCtrl", {$scope: scope, youTubeDataAPI: youTubeDataAPI});
-    }
-  ));
+      ctrl = $controller("SearchboxCtrl", {$scope: scope});
+      scope.ctrl = ctrl;
 
-  it ('should query youTubeDataAPI with the current search box contents when the spyglass button is clicked', function(){
+      form = angular.element(
 
-  })
+        "<form ng-controller='SearchboxCtrl as ctrl'><md-autocomplete flex search-box-submit-on-return placeholder='Search YouTube'" +
+        "md-selected-item='ctrl.selectedItem' md-search-text='ctrl.searchText' md-search-text-change='ctrl.textChange()'" +
+        "md-selected-item-change='ctrl.submit(ctrl.selectedItem.value)' md-items='item in ctrl.getSuggestions(ctrl.searchText)'" + 
+        "md-item-text='item.value'></form>");
 
-  it ('should query youTubeDataAPI with search string when a selection is made from the autoComplete dropdown', function(){
+      searchButton = angular.element("<div><md-button ng-click='ctrl.submit(ctrl.searchText)'><md-icon></md-icon></md-button><div>");
+      
+      $compile(searchButton)(scope);
+      $compile(form)(scope);
 
-  })
+      scope.$digest();
+      spyOn(ctrl.youTube, 'query');
 
-  it ('should query youTubeDataAPI with the current search box contents when carriage return is hit', function(){
+    }));
 
-  })
+    it ('should query youTubeDataAPI with the current search box contents when the spyglass button is clicked', function(){
 
-  it ('should not query youTubeDataAPI with an empty string', function(){
+      ctrl.searchText = "taylor swift";
+      ctrl.submit(ctrl.searchText);
+      expect(ctrl.youTube.query).toHaveBeenCalledWith('taylor swift');
+    });
 
-  })
+    it ('should query youTubeDataAPI with suggestion string when a selection is made from the dropdown', function(){
+      var elem = form.find('md-autocomplete');
+      elem.scope().ctrl.selectedItem = { value:'nikki' };
+      elem.scope().$apply();
+      expect(ctrl.youTube.query).toHaveBeenCalledWith('nikki');
+    });
 
-  describe('SearchboxCtrl.getSuggestions', function(){
+    it ('should query youTubeDataAPI with the current search box contents on carriage return', function(){
+      
+      var elem = form.find('md-autocomplete');
+      elem.scope().ctrl.searchText = 'taylor swift';
+
+      event = jQuery.Event('keypress');
+      event.which = 13;
+      elem.trigger(event);
+      elem.scope().$apply();
+
+      expect(ctrl.youTube.query).toHaveBeenCalledWith('taylor swift');
+    });
+
+
+    it ('should not query youTubeDataAPI with an empty string', function(){
+      ctrl.submit('');
+      expect(ctrl.youTube.query).not.toHaveBeenCalled();
+    });
+
+  });
+
+  describe('SearchboxCtrl.getSuggestions: fetching data from googleSuggestionAPI', function(){
 
     var scope, ctrl, httpBackend, results, query;
     
@@ -44,12 +86,12 @@ describe('Controller: SearchboxCtrl', function () {
           ["taylor swift i knew you were trouble",0],["taylor swift out of the woods",0],
           ["taylor swift 22",0]],{"k":1,"q":"nUelgMSxSWzhhaN_y0fPq6zCVyI"}];
         
-        var url = "https://suggestqueries.google.com/complete/search?callback=JSON_CALLBACK&client=youtube&ds=yt&hl=en&q=taylor+swift"
+        var url = "https://suggestqueries.google.com/complete/search?callback=JSON_CALLBACK&client=youtube&ds=yt&hl=en&q=taylor+swift";
         httpBackend.whenJSONP(url).respond(mockData);
 
         // Execute query request.
         ctrl.getSuggestions(query).then(function(response){ 
-          results = response 
+          results = response; 
         });
       
         httpBackend.flush();
