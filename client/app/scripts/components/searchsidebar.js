3e0aec189662219f6a72e483487a67a51e90589b
cgewecke <christophergewecke@gmail.com>
2015-05-06 13:08:14 -0700

    Search sidebar unit tests, refactor of search side bar code into component form

--- client/app/scripts/components/searchsidebar.js
+++ client/app/scripts/components/searchsidebar.js
@@ -15,14 +15,13 @@ var sb_debug, sb_debugII, sb_debugIII;
 
   angular.module('embeditor.components.searchsidebar', ['embeditor.services.youTubeDataAPI', 'embeditor.services.youtubePlayerAPI'])
     
-    .controller('SearchSidebarCtrl', searchSidebarCtrl)
-    .directive('embeditorSearchHistoryOption', searchHistoryOption)
-    .directive('embeditorSearchPlayVideo', searchPlayVideo )
-    .directive('embeditorSearchThumbnailChecker', searchThumbnailChecker);
+    .controller('SearchSidebarCtrl', SearchSidebarCtrl)
+    .directive('embeditorSearchHistoryOption', embeditorSearchHistoryOption)
+    .directive('embeditorSearchItem', embeditorSearchItem )
 
     // Controller for the sidebar. Makes youtube api visible on scope and
     // toggles sidebar open when a query is made from toolbar.
-    function searchSidebarCtrl($scope, youTubeDataAPI, $mdSidenav) {
+    function SearchSidebarCtrl($scope, youTubeDataAPI, $mdSidenav) {
      
       var self = this;
       self.youTube = youTubeDataAPI;
@@ -30,24 +29,26 @@ var sb_debug, sb_debugII, sb_debugIII;
 
       // Toggles sidebar open if closed
       $scope.$on('youTubeDataAPI:query', function(event, msg){
+        console.log('heard query at sidebar');
         if (!self.mdSidenav('search').isOpen()){
+          console.log('entered condition at sidebar');
           self.mdSidenav.exists = true; // exception issues for toolbar.js
           self.mdSidenav('search').toggle();
         }
       });
     };
-    searchSidebarCtrl.$inject = ['$scope', 'youTubeDataAPI', '$mdSidenav'];
+    SearchSidebarCtrl.$inject = ['$scope', 'youTubeDataAPI', '$mdSidenav'];
 
     // Located on each option of the history select. This helps the select work
     // like a menu with links. We need the history fetch to occur regardless of
     // whether it was the last history selection made, because many other searches could
     // have been run in the interim. 
-    function searchHistoryOption(youTubeDataAPI){
+    function embeditorSearchHistoryOption(youTubeDataAPI){
       return {
         restrict: 'A',
-        link: submitEventHandlers
+        link: historyOptionEventHandlers
       };
-      function submitEventHandlers(scope, elem, attrs){
+      function historyOptionEventHandlers(scope, elem, attrs){
             
         elem.bind('click', function(event){
            youTubeDataAPI.getAgain(scope.previous);
@@ -60,38 +61,74 @@ var sb_debug, sb_debugII, sb_debugIII;
         });
       };
     };
-    searchHistoryOption.$inject = ['youTubeDataAPI'];
+    embeditorSearchHistoryOption.$inject = ['youTubeDataAPI'];
 
-    // Located on the play button - click closes the sidebar and
-    // loads the search result into the player
-    function searchPlayVideo(youtubePlayerAPI, $mdSidenav){
+    // <embeditor-search-item></embeditor-search-item>
+    function embeditorSearchItem(youTubeDataAPI, youtubePlayerAPI, $mdSidenav){
       return{
-        restrict: 'A',
-        link: clickEventHandler
+        restrict: 'E',
+        scope: { video: '=video' },
+        link: searchItemEventHandlers,
+        template: '\
+          <div class="searchItems">\
+            <span class="searchItemDuration">{{video.duration}}</span>\
+            <span class="searchItemPublishedAt">{{video.publishedAt}}</span>\
+            <div class="searchItemClickArea"\
+                 ng-init="hover=false"\
+                 ng-mouseenter="hover=true"\
+                 ng-mouseleave="hover=false">\
+                 <span class="fa fa-play-circle fa-3x searchPlayIcon"\
+                       ng-show="hover"></span>\
+                 <md-button\
+                    aria-label="related"\
+                    class="searchRelatedIcon"\
+                    ng-show="hover"\
+                    ng-click="search.youTube.getRelatedVideos(video); $event.stopPropagation()">\
+                    <md-tooltip\
+                      class="searchRelatedTooltip searchRelatedText"\
+                      md-direction="left">\
+                      <span class="searchRelatedText"> Related </span> \
+                    </md-tooltip>\
+                    <md-icon md-font-icon="fa fa-sitemap"></md-icon>\
+                </md-button>\
+                <img class="searchThumbnail"\
+                     ng-src="{{video.imageUrl}}">\
+            </div>\
+            <div class="searchItemTitleContainer">{{video.title}}</div>\
+            <div class="searchItemChannelContainer"> by {{video.channelTitle}}</div>\
+          </div>'
       };
-      function clickEventHandler(scope, elem, attrs){
-        elem.bind('click', function(event){
-          $mdSidenav('search').toggle();
-          youtubePlayerAPI.load(scope.video.videoId);
-        });
-      }
-    }
-    searchPlayVideo.$inject = ['youtubePlayerAPI', '$mdSidenav'];
 
-    // Not implemented. In chrome, a 404 event when the thumbnail 
-    // link is broken, but it never triggers the 'error' event. Youtube
-    // also sends a small 1mb default image that rarely loads. Firefox
-    // successfully loads this image.
-    function searchThumbnailChecker($http){
-      return {
-        restrict: 'A',
-        link: loadErrorEventHandler
-      };
-      function loadErrorEventHandler(scope, elem, attrs){
+      function searchItemEventHandlers(scope, elem, attrs){
+
+        var dataAPI = youTubeDataAPI;
+        var playerAPI = youtubePlayerAPI
+        var thumbnailElem = angular.element(elem.find('div')[1]);
+        var channelElem = angular.element(elem.find('div')[3]);
+        var relatedElem = elem.find('button');
+
+        // Play Video click
+        thumbnailElem.bind('click', function(event){
+          //$mdSidenav('search').toggle();
+          console.log("video: " + scope.video.videoId);
+          playerAPI.load({videoId: scope.video.videoId});
+        });
+
+        // Search Related Videos click
+        relatedElem.bind('click', function(event){
+          dataAPI.getRelatedVideos(scope.video); 
+          event.stopPropagation();
+        });
+
+        // Search Channel Videos click
+        channelElem.bind('click', function(event){
+          dataAPI.getChannelVideos(scope.video);
+        });
       };
     };
-    searchThumbnailChecker.$inject = ['$http'];
+    embeditorSearchItem.$inject = ['youTubeDataAPI', 'youtubePlayerAPI', '$mdSidenav'];
+
+    
 
 })();
-        
-
+      
