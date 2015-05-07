95b20d72f26d1aaa9cd877db89298ae0a44e6189
cgewecke <christophergewecke@gmail.com>
2015-05-07 16:37:48 -0700

    More sidebar unit tests, added ng-md-icons

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
             'bower_components/angular-material/angular-material.js',
             'bower_components/angular-mocks/angular-mocks.js',
             // endbower
+      "app/fonts/*.js",
       "app/scripts/**/*.js",
       'app/templates/*.html',
       "test/mock/**/*.js",
